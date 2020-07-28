f679321d2df0faf5dc7215804bd213fee32b72a2
ThaisDaSilva <46365312+ThaisDaSilva@users.noreply.github.com>
2020-07-27 17:44:36 -0700

    phase1

--- Codelab-onlinelessonsWeb/index.js
+++ Codelab-onlinelessonsWeb/index.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+/* document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("button1").addEventListener("click", function () {
 
         document.getElementById("textbox1").value = "success";
     });
-});
+}); */
